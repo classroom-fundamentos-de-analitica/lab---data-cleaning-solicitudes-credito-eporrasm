742820d28ba7f579cf10483acfde8f91f2b4901b
eporrasm <vals4oo@gmail.com>
2023-02-20 16:18:49 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -13,8 +13,21 @@ def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
 
-    #
-    # Inserte su código aquí
-    #
+    df = df.drop('Unnamed: 0', axis = 1)
+    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio, dayfirst=True)
+
+    df.monto_del_credito = df.monto_del_credito.apply(lambda x: str(x).strip('$').replace(',',''))
+    df.monto_del_credito = df.monto_del_credito.astype(float)
+    df.monto_del_credito = df.monto_del_credito.astype(int)
+
+    for columna in ['sexo','tipo_de_emprendimiento',
+                    'idea_negocio','barrio','línea_credito']:
+        df[columna] = df[columna].apply(
+                                        lambda s: s.lower().replace('-',' ').replace('_',' ') 
+                                        if type(s) == str else s)
+    df.dropna(axis=0, inplace = True)
+    df.drop_duplicates(inplace=True)
 
     return df
+
+print(clean_data().sexo.value_counts().to_list())
